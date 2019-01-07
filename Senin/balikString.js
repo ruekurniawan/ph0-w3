280b58ce748438857312373faff35ce825f193ca
ruekurniawan <ruekurniawan>
2019-01-07 14:56:53 +0700

    Tugas Senin

---
+++ Senin/balikString.js
@@ -0,0 +1,22 @@
+function balikString(str)
+{
+	var result = ""
+	for(let i = str.length-1; i >= 0; i--)
+	{
+		result += str[i];
+	}
+	return result;
+}
+
+// function balikString(str)
+// {
+// 	var result = "";
+// 	var indeks = str.length-1
+// 	while(indeks >= 0)
+// 	{
+// 		result += str[indeks]
+// 		indeks--;
+// 	}
+// 	return result;
+// }
+// console.log(balikString("Hello World!"))
---
+++ Senin/builtinFunction.js
@@ -0,0 +1,82 @@
+function dataHandling2(input)
+{
+	input.splice(1,2, "Roman Alamsyah Elsharawy", "Provinsi Bandar Lampung");
+	input.splice(4,1, "Pria", "SMA Internasional Metro");
+	console.log(input)
+	var bulan = input[3].split("/");
+	switch(bulan[1])
+	{
+		case "01" : 
+		{
+			console.log("Januari");break;
+		}
+		case "02" : 
+		{
+			console.log("Februari");break;
+		}
+		case "03" : 
+		{
+			console.log("Maret");break;
+		}
+		case "04" : 
+		{
+			console.log("April");break;
+		}
+		case "05" : 
+		{
+			console.log("Mei");break;
+		}
+		case "06" : 
+		{
+			console.log("Juni");break;
+		}
+		case "07" : 
+		{
+			console.log("Juli");break;
+		}
+		case "08" : 
+		{
+			console.log("Agustus");break;
+		}
+		case "09" : 
+		{
+			console.log("September");break;
+		}
+		case "10" : 
+		{
+			console.log("Oktober");break;
+		}
+		case "11" : 
+		{
+			console.log("November");break;
+		}
+		case "12" : 
+		{
+			console.log("Desember");break;
+		}
+		default : 
+		{
+			console.log("Salah bulan");
+		}
+	}
+	// input[3].split("/").sort(function (value1,value2) {return value2 - value1})
+	console.log(input[3].split("/").sort(function (value1,value2) {return value2 - value1}))
+	console.log(input[3].split("/").join("-"));
+	if(typeof(input[1]) === "string")
+	{
+		console.log(input[1].slice(0,15));
+	}
+}
+
+var input = ["0001", "Roman Alamsyah ", "Bandar Lampung", "21/05/1989", "Membaca"];
+dataHandling2(input);
+
+/**
+ * keluaran yang diharapkan (pada console)
+ *
+ * ["0001", "Roman Alamsyah Elsharawy", "Provinsi Bandar Lampung", "21/05/1989", "Pria", "SMA Internasional Metro"]
+ * Mei
+ * ["1989", "21", "05"]
+ * 21-05-1989
+ * Roman Alamsyah
+ */
